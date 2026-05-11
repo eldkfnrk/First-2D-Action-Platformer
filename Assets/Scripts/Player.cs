1a9b5e098c99befbe50a048529624f86542b3c89
eldkfnrk <johongjin01@gmail.com>
2026-05-11 16:20:31 +0900

    점프 문제 해결, 애니메이터 연결, 공격 추가
    일정 횟수 이상 점프 시 점프가 안 되는 문제를 해결.(y축 속도 이용과 ray길이 줄이기)
    적절한 상황에 적절한 애니메이션 동작하도록 애니메이터 연결.
    공격 기능 추가.(키 입력 시 애니메이션 동작까지만)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,26 +8,36 @@ public class Player : MonoBehaviour
     public float jumpPower;
     float direction;
 
-    bool isJump;
+    public LayerMask groundLayer;
+    RaycastHit2D jumpCheck;
+    bool isGround;
+    bool isAttack;
 
     Rigidbody2D rigid;
     SpriteRenderer spriteR;
+    Animator animator;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriteR = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        isGround = true;
+        animator.SetBool("Grounded", isGround);
     }
 
     private void Update()
     {
-        Debug.DrawRay(transform.position, Vector2.down, Color.red, 0.5f);
+        animator.SetFloat("AirSpeedY", rigid.linearVelocityY);
 
-        RaycastHit2D jumpCheck = Physics2D.Raycast(transform.position, Vector2.down, 0.5f, LayerMask.GetMask("Ground"));
-
-        if (jumpCheck.collider != null)
+        if(rigid.linearVelocityY <= 0)
         {
-            isJump = false;
+            jumpCheck = Physics2D.Raycast(transform.position, Vector2.down, 0.05f, groundLayer);
+            if (jumpCheck.collider != null && !isGround)
+            {
+                isGround = true;
+                animator.SetBool("Grounded", isGround);
+            }
         }
     }
 
@@ -40,7 +51,22 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (isAttack)
+        {
+            rigid.linearVelocityX = 0f;
+            return;
+        }
+
         rigid.linearVelocityX = direction * moveSpeed;
+
+        if (direction != 0f)
+        {
+            animator.SetInteger("AnimState", 1);
+        }
+        else
+        {
+            animator.SetInteger("AnimState", 0);
+        }
     }
 
     void OnMove(InputValue value)
@@ -50,10 +76,30 @@ public class Player : MonoBehaviour
 
     public void OnJump(InputValue value)
     {
-        if (!isJump)
+        if (isGround)
         {
-            isJump = true;
+            isGround = !isGround;
+            animator.SetBool("Grounded", isGround);
+            animator.SetTrigger("Jump");
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
+    }
+
+    void OnAttack(InputValue value)
+    {
+        if (!isAttack && isGround)
+        {
+            StartCoroutine(AttackRoutine());
+        }
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        isAttack = true;
+        animator.SetTrigger("Attack1");
+
+        yield return new WaitForSeconds(0.5f);
+
+        isAttack = false;
     }
 }
