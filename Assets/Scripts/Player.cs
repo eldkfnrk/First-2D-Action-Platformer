dc87b1198b9270d87d725764457a891d5e0c2c8e
eldkfnrk <johongjin01@gmail.com>
2026-05-16 00:05:57 +0900

    연속 공격 시스템 생성
    기존 코드에서 공격 횟수를 저장하는 변수를 추가하여 공격이 이어지도록 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     RaycastHit2D jumpCheck;
     public bool isGround;
     bool isAttack;
+    int attackCount;  // 공격 횟수를 저장할 변수
 
     Rigidbody2D rigid;
     SpriteRenderer spriteR;
@@ -29,6 +30,16 @@ public class Player : MonoBehaviour
 
     float boxDirection;  // 플레이어 기준 overlapbox가 나올 방향(-방향인지 +방향인지)
     Vector2 boxPosition;  // overlapbox가 생기는 위치
+
+    enum AttackState
+    {
+        Attack1,
+        Attack2,
+        Attack3,
+        None,
+    }
+
+    AttackState attackState;
 
     private void Awake()
     {
@@ -109,33 +120,71 @@ public class Player : MonoBehaviour
 
     void OnAttack(InputValue value)
     {
+        // J키 입력 - 첫 번째 공격
+        // 첫 번째 공격 중 J키 입력 - 두 번째 공격
+        // 두 번째 공격 중 J키 입력 - 세 번째 공격
+        // 공격이 끝날 때까지 후속 입력이 없다면 해당 공격 종료 후 초기화
         if (!isAttack && isGround)
         {
+            ++attackCount;
             boxDirection = spriteR.flipX ? -1f : 1f;
-            StartCoroutine(AttackRoutine(boxDirection));
+            StartCoroutine(AttackRoutine());
+        } else if (isAttack && attackCount < 3)
+        {
+            ++attackCount;
         }
     }
 
-    IEnumerator AttackRoutine(float boxDirection)
+    IEnumerator AttackRoutine()
     {
         isAttack = true;
-        animator.SetTrigger("Attack1");
         boxPosition.x = transform.position.x + boxDirection;
         boxPosition.y = transform.position.y;
-        attackedEnemy = Physics2D.OverlapBox(boxPosition, boxSize, 0f, enemyLayer);
-
-        if(attackedEnemy != null)
-        {
-            attackedEnemy.GetComponent<Enemy>().curHp -= 5f;
-        }
+        attackState = AttackState.Attack1;
+        AttackEnemy();
 
         m_Started = true;
 
         yield return attackDelay;
 
+        if (attackCount >= 4)
+        {
+            Debug.Log("입력 제어 실패");
+            attackCount = 0;
+            m_Started = false;
+            isAttack = false;
+            StopCoroutine(AttackRoutine());
+        }
+
+        if (attackCount >= 2)
+        {
+            attackState = AttackState.Attack2;
+            AttackEnemy();
+            yield return attackDelay;
+        }
+
+        if (attackCount == 3)
+        {
+            attackState = AttackState.Attack3;
+            AttackEnemy();
+            yield return attackDelay;
+        }
+
+        attackCount = 0;
         m_Started = false;
 
         isAttack = false;
+    }
+
+    void AttackEnemy()
+    {
+        animator.SetTrigger(attackState.ToString());
+        attackedEnemy = Physics2D.OverlapBox(boxPosition, boxSize, 0f, enemyLayer);
+
+        if (attackedEnemy != null)
+        {
+            attackedEnemy.GetComponent<Enemy>().curHp -= 5f;
+        }
     }
 
     // overlapbox의 범위를 씬 화면에 그려주기 위해 호출한 함수(함수 내에서 설정한 기즈모를 그려주는 함수로 추정된다.)
