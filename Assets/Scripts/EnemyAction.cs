c00486cdd06dd0472010406eb101f9ace5daa4c2
eldkfnrk <johongjin01@gmail.com>
2026-06-16 23:57:50 +0900

    적 애니메이터 설정. 씬 추가 등 수행.
    적으로 사용할 에셋의 애니메이터를 내가 사용할 형태로 수정.(트랜지션이 하나도 없이 애니메이션만 있는 상태라 각 애니메이션들을 연결)
    보스전 씬과 일반 씬을 나누기 위해 씬 하나 추가.
    게임을 관리하는 게임 매니저 추가.
    각 오브젝트 프리팹으로 관리하도록 프리팹 폴더 생성 후 오브젝트 프리팹으로 변경.

--- Assets/Scripts/EnemyAction.cs
+++ Assets/Scripts/EnemyAction.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class EnemyAction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum EnemyState
     {
-        
+        Idle,
+        Run,
+        Fall,
+        Dash,
+        DashAttack,
+        Attack,
+        Slide,
+        Hurt,
+        Croush,
+        Jump,
+        Death,
+    }
+
+    EnemyState state;
+
+    private void Awake()
+    {
+        state = EnemyState.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 보스전이 시작하기 전까지는 Idle
+        // 보스전 시작 후 각 동작 수행
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // 구역을 저장하는 열거형
+    public enum Area
+    {
+        // 씬 순서와 구역을 일치시키면 현재 플레이어가 있는 구역을 저장할 수 있을 것으로 기대
+        NormalStage,
+        BossStage,
+    }
+
+    public Area area;
+
+    private void Awake()
+    {
+        int curSceneNum = SceneManager.GetActiveScene().buildIndex;
+
+        switch (curSceneNum)
+        {
+            case 0:
+                area = Area.NormalStage;
+                break;
+            case 1:
+                area = Area.BossStage;
+                break;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
